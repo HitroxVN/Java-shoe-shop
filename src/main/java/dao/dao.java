dd4682f5032acfa1a0ecb6eb8a0c217705c3ad36
HitroxVN <pt91563@gmail.com>
2025-11-20 10:21:38 +0700

    Second commit

---
+++ src/main/java/dao/dao.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Hoàng Văn Trường
+ * Created on: Nov 20, 2025
+ */
+package dao;
+
+public class dao {
+
+	public dao() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/models/model.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Hoàng Văn Trường
+ * Created on: Nov 20, 2025
+ */
+package models;
+
+public class model {
+
+	public model() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
