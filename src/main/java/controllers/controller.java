6fca1ef80348f1e7ceaa4a63c3b0c2b50b9d1574
HitroxVN <pt91563@gmail.com>
2025-11-20 10:21:38 +0700

    first commit

---
+++ src/main/java/controllers/controller.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Hoàng Văn Trường
+ * Created on: Nov 20, 2025
+ */
+package controllers;
+
+public class controller {
+
+	public controller() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
